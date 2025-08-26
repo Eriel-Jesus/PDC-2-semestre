f2defd10b485d296eb272801515a7240be610eea
Eriel-Jesus <83691312+Eriel-Jesus@users.noreply.github.com>
2025-08-25 21:13:51 -0300

    Add files via upload

---
+++ Exerc1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo(5,10);
+
+            Console.WriteLine("Insira o valor da base: ");
+            retangulo.setBase(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Insira o valor da altura: ");
+            retangulo.setAltura(int.Parse(Console.ReadLine()));
+
+            retangulo.Calcular();
+
+            Console.Clear();
+
+            Console.WriteLine("Base: {0}", retangulo.getBase());
+            Console.WriteLine("Altura: {0}", retangulo.getAltura());
+            Console.WriteLine("Area: {0}", retangulo.getArea());
+
+        }
+    }
+}
---
+++ Exerc1/Retangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc1
+{
+    internal class Retangulo
+    {
+         int b;
+         int h;
+         int area;
+
+        public Retangulo()
+        {
+            b = 0;
+            h = 0;
+        }
+
+        public Retangulo(int b, int h)
+        {
+            this.b = b;
+            this.h = h;
+        }
+
+        public void setBase(int b)
+        {
+            this.b = b;
+        }
+
+        public void setAltura(int h)
+        {
+            this.h = h;
+        }
+
+        public int getBase()
+        {
+            return this.b;
+        }
+
+        public int getAltura()
+        {
+            return this.h;
+        }
+
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        public void Calcular()
+        {
+            this.area = this.b * this.h;
+        }
+
+    }
+}
